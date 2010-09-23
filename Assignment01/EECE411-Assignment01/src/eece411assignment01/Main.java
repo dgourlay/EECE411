38c3534b3941b1e6ddb5ad62140aa2ba57e5d795
Derek <derekgourlay@gmail.com>
2010-09-23 00:36:08 -0700

    Assignment finished / working

--- Assignment01/EECE411-Assignment01/src/eece411assignment01/Main.java
+++ Assignment01/EECE411-Assignment01/src/eece411assignment01/Main.java
@@ -1,88 +1,112 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  UBC EECE 411 - Assignment 01
+ *
+ *  Author: Derek Gourlay, 66435041
+ *  Date: Thursday, September 23, 2010
+ *
+ * Usage, call from commandline with arguements: server address, port number, student number
+ *
  */
 package eece411assignment01;
 
 import eece411assignment01.util.ByteOrder;
 import eece411assignment01.util.StringUtils;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketException;
 
-/**
- *
- * @author dgourlay
- */
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
+    private static int DEFAULT_PORT = 5627;
+
     public static void main(String[] args) throws IOException {
+
         String server = args[0];       // Server name or IP address
-        int servPort = (args.length == 3) ? Integer.parseInt(args[1]) : 5627;
-        int studentNum = (args.length == 3) ? Integer.parseInt(args[2]) : Integer.parseInt(args[1]);
-        //byte[] byteBuffer = (args.length == 3) ? args[2].getBytes() : args[1].getBytes();
+        int servPort = (args.length == 3) ? Integer.parseInt(args[1]) : DEFAULT_PORT; //user specified port or default #
+        int studentNum = (args.length == 3) ? Integer.parseInt(args[2]) : Integer.parseInt(args[1]); //student num to send to server
 
 
         System.out.println("Attempting to connect to " + server + ":" + servPort + " ...");
+
         // Create socket that is connected to server on specified port
-        Socket socket = new Socket(server, servPort);
+
+        Socket socket = null;
+        try {
+            socket = new Socket(server, servPort);
+        } catch (IOException iOException) {
+            System.out.println("Error setting up socket connection...");
+            iOException.printStackTrace();
+
+        }
+
         System.out.println("Successfully connected to server " + server + " (" + socket.getInetAddress() + ")!");
 
-
+        //Setup in/out streams to/from the server
         InputStream in = socket.getInputStream();
         OutputStream out = socket.getOutputStream();
 
+        //send the server the studentNumber in little endian format
         ByteOrder.int2leb(studentNum, out);
 
-        /*
-        byte[] response = new byte[100];
-        int byteCount=0;
-        while(in.read(response) != -1){
-            byteCount++;
-        }
-         *
-         */
 
+        //Get the message size and code length
+        byte[] responseBuffer = new byte[12];
 
-        BufferedReader inReader = new BufferedReader(new InputStreamReader(
-                in));
+        int totalBytesRcvd = 0;  // Total bytes received so far
+        int bytesRcvd;           // Bytes received in last read
 
-        byte[] byteBuffer = {};
-
-        String responseString = inReader.readLine();
-        byte[] responseBuffer = responseString.getBytes();
-
-        
-        int msgLength = ByteOrder.leb2int(responseBuffer, 0, 4);
-
-        System.out.println("Message length: " + msgLength);
-
-        int codeLength = ByteOrder.leb2int(responseBuffer, 8, 4);
-        System.out.println("Code length: " + codeLength);
-
-        byte[] codeArray = new byte[codeLength];
-
-        int index=0;
-        for(int i = msgLength - codeLength -1; i < msgLength; i++,index++){
-            codeArray[index]=responseBuffer[i];
+        while (totalBytesRcvd < responseBuffer.length) {
+            if ((bytesRcvd = in.read(responseBuffer, totalBytesRcvd,
+                    responseBuffer.length - totalBytesRcvd)) == -1) {
+                throw new SocketException("Connection close prematurely");
+            }
+            totalBytesRcvd += bytesRcvd;
         }
 
+        int msgLength = ByteOrder.leb2int(responseBuffer, 0);
+        int codeLength = ByteOrder.leb2int(responseBuffer, 8);
 
-        System.out.println(StringUtils.byteArrayToHexString(codeArray));
-        
-        //int secretCode = ByteOrder.leb2int(responseBuffer, responseBuffer.length - codeLength);
+        System.out.println("Message Length: " + msgLength);
+        System.out.println("Code Length: " + codeLength);
+
+        //get remaining portion of the message
+        byte[] remainBuffer = new byte[msgLength - 12];
+        totalBytesRcvd = 0;
+        bytesRcvd = 0;
+
+        while (totalBytesRcvd < remainBuffer.length) {
+            if ((bytesRcvd = in.read(remainBuffer, totalBytesRcvd,
+                    remainBuffer.length - totalBytesRcvd)) == -1) {
+                throw new SocketException("Connection close prematurely");
+            }
+            totalBytesRcvd += bytesRcvd;
+        }
+
+        //extract the code
+        byte[] codeBuffer = new byte[codeLength];
+
+        for (int i = 0, index = remainBuffer.length - codeLength; i < codeLength; i++, index++) {
+            codeBuffer[i] = remainBuffer[index];
+        }
+
+        String code = StringUtils.byteArrayToHexString(codeBuffer);
+        System.out.println("The secret code is: " + code);
 
 
-        //      System.out.println("Code: "+codeString);
 
-         
-        socket.close();  // Close the socket and its streams
+        try {
+            // Close the socket and its streams
+            socket.close();
+        } catch (IOException iOException) {
+            System.out.println("Error closing socket...");
+            iOException.printStackTrace();
+        }
 
     }
 }
